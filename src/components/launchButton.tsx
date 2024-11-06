97df0ee5f5106962b50594d4f22e731ec1d78a3f
DanInDev <dnlnielsen102@gmail.com>
2024-11-06 20:05:46 +0100

    Mock Exam Ready

--- src/components/launchButton.tsx
+++ src/components/launchButton.tsx
@@ -1,40 +1,14 @@
-import React, { useState } from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import StarText from "./star_components/starText";
-import colors from "../constants/colors";
-
-/* This should be removed for the example */
 interface LaunchButtonProps {
-  starshipName: string;
-  onLaunch?: () => void;
 }
 
 const LaunchButton: React.FC<LaunchButtonProps> = ({
-  starshipName,
-  onLaunch,
-}) => {
-  const [launched, setLaunched] = useState(false);
+ // May the force be with you!
 
-  const handleLaunchPress = () => {
-    setLaunched(true);
-    onLaunch?.();
-    console.log(`Starship ${starshipName} launched!`);
-  };
+}) => {
   return (
-    <TouchableOpacity onPress={handleLaunchPress} style={styles.launchButton}>
-      <StarText>{launched ? "Launched" : "Ready"}</StarText>
-    </TouchableOpacity>
   );
 };
 
-const styles = StyleSheet.create({
-  launchButton: {
-    padding: 10,
-    borderRadius: 5,
-    backgroundColor: colors.danger,
-    alignItems: "center",
-  },
-});
 
 export default LaunchButton;
 
--- src/screens/emergencyScreen.tsx
+++ src/screens/emergencyScreen.tsx
@@ -1,1 +1,13 @@
-// May the force be with you!
+
+import { View, Text } from "react-native";
+
+const EmergencyScreen = () => {
+  // May the force be with you!
+
+  return (
+    <View>
+      <Text>EmergencyScreen</Text>
+    </View>
+  );
+};
+export default EmergencyScreen;
